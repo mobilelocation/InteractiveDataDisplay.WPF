771bbcc1fd807a5f90cd550aab92882fe5b0985f
zhoupn138 <p.zhou@procept-biorobotics.com>
2019-04-27 22:28:37 -0700

    Convert the linegraphsample to MVVM.

---
+++ samples/LineGraphSampleMvvm/ViewModel/LineGraphViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace LineGraphSampleMvvm.ViewModel
+{
+    public class LineGraphViewModel : ViewModelBase
+    {
+        private Brush _stroke;
+        private string _description;
+        private double _strokeThickness;
+        private PointCollection _points;
+
+        public Brush Stroke { get => _stroke; set => Set(ref _stroke, value); }
+
+        public string Description { get => _description; set => Set(ref _description, value); }
+
+        public double StrokeThickness { get => _strokeThickness; set => Set(ref _strokeThickness, value); }
+
+        public PointCollection Points { get => _points; set => Set(ref _points, value); }
+    }
+}
---
+++ samples/LineGraphSampleMvvm/ViewModel/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace LineGraphSampleMvvm.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private ObservableCollection<LineGraphViewModel> _lineGraphViewModels;
+        private string _title;
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            var lineGraphVms = new List<LineGraphViewModel>();
+
+            double[] x = new double[200];
+            for (int i = 0; i < x.Length; i++)
+                x[i] = 3.1415 * i / (x.Length - 1);
+
+            for (int i = 0; i < 25; i++)
+            {
+                var lg = new LineGraphViewModel
+                {
+                    Stroke = new SolidColorBrush(Color.FromArgb(255, 0, (byte)(i * 10), 0)),
+                    Description = string.Format("Data series {0}", i + 1),
+                    StrokeThickness = 2,
+                    Points = new PointCollection(x.Select(v => new Point(v, Math.Sin(v + i / 10.0))))
+                };
+
+                lineGraphVms.Add(lg);
+            }
+
+            LineGraphViewModels = new ObservableCollection<LineGraphViewModel>(lineGraphVms);
+
+            Title = "Line graph legnend sample MVVM";
+        }
+
+        /// <summary>
+        /// The collection of ViewModels for the LineGraphs.
+        /// </summary>
+        public ObservableCollection<LineGraphViewModel> LineGraphViewModels { get => _lineGraphViewModels; set => Set(ref _lineGraphViewModels, value); }
+
+        /// <summary>
+        /// The title
+        /// </summary>
+        public string Title { get => _title; set => Set(ref _title, value); }
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
